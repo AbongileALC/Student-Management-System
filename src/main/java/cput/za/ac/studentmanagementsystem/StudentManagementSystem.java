d2f2c0544b3f397f27c2ab82b271875a425e8613
abong <abong@abongile>
2026-02-14 22:46:18 +0200

    testing

---
+++ src/main/java/cput/za/ac/studentmanagementsystem/StudentManagementSystem.java
@@ -0,0 +1,12 @@
+package cput.za.ac.studentmanagementsystem;
+
+/**
+ *
+ * @author abong
+ */
+public class StudentManagementSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
